754338bdc1ad33a30928c6e73ce3526d4d17eb2d
dev-jb <bajracharyajenish@gmail.com>
2024-07-15 14:48:30 +0545

    feat: update op-node run script

--- scripts/start-op-node.sh
+++ scripts/start-op-node.sh
@@ -6,11 +6,12 @@ exec op-node \
   --l1=$OP_NODE__RPC_ENDPOINT \
   --l2=$L2_RPC_WS_URL \
   --rpc.addr=0.0.0.0 \
-  --rpc.port=$PORT__OP_NODE_RPC \
+  --rpc.port=$PORT__OP_NODE_HTTP \
   --rollup.config=/chainconfig/rollup.json \
   --l2.jwt-secret=/shared/jwt.txt \
   --l1.trustrpc \
   --l1.rpckind=$OP_NODE__RPC_TYPE \
+  --p2p.listen.tcp=$PORT__OP_NODE_P2P \
   --l1.beacon=$OP_NODE__L1_BEACON \
   --syncmode=execution-layer \
   $EXTENDED_ARG $@
